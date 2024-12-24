5e9928f626194129bf3a9afbc85fe02776e8b5a5
Brian Yang <yangbri1@msu.edu>
2024-12-24 00:30:15 +0000

    Surprised the database table 'song' worked w/o explicitly declaring the columns w/ constraints of 'NOT NULL'

--- problem1.sql
+++ problem1.sql
@@ -1,9 +1,9 @@
 CREATE TABLE song(
-    title VARCHAR(100),
-    artist VARCHAR(100)
+    title VARCHAR(100) NOT NULL,
+    artist VARCHAR(100) NOT NULL
 );
 --  create an table 'song' w/ two columns 'title' & 'artist'  of (character, fixed-length -- no whitespace padding for extra spaces) data type VARCHAR(100) --
-
+-- 'NOT NULL' constraints are optional here it seems works w/ or w/o
 /**
      * Assignment: Create a new table in the problem1.sql file, called "song" with 2 columns "title" and "artist".
      * both columns should have the datatype varchar(100), which represents a String of up to 100 characters.
