cea03a6debd929c4faa0bb619174e14240464b94
Brian Yang <yangbri1@msu.edu>
2024-12-24 00:28:30 +0000

    Created a DB table to preserve values unlike when data is placed in variables (gone when instance ends)

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,20 @@
+CREATE TABLE song(
+    title VARCHAR(100),
+    artist VARCHAR(100)
+);
+--  create an table 'song' w/ two columns 'title' & 'artist'  of (character, fixed-length -- no whitespace padding for extra spaces) data type VARCHAR(100) --
 
+/**
+     * Assignment: Create a new table in the problem1.sql file, called "song" with 2 columns "title" and "artist".
+     * both columns should have the datatype varchar(100), which represents a String of up to 100 characters.
+     *
+     *     Example Song Table Diagram:
+     *     |      title        |        artist         |
+     *     ---------------------------------------------
+     *     |'Let it be'        |'Beatles'              |
+     *     |'Hotel California' |'Eagles'               |
+     *     |'Kashmir'          |'Led Zeppelin'         |
+     *
+     *     Do not change anything in this code. You should write your sql statement on a single line in the
+     *     problem1.sql file.
+     */
